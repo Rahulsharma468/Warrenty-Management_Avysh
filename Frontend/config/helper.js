6b40aaaa065594f38dd741a0b1f46627d2bc3fe2
rahul Sharma <sharmarahul1729@gmail.com>
2021-07-14 21:03:39 +0530

    Nodemailer

--- Frontend/config/helper.js
+++ Frontend/config/helper.js
@@ -1,36 +1,37 @@
 const schedule = require("node-schedule");
+const { sendNotification } = require('./mail');
 
-const scheduleNotification = async (item) => {
-  let expDate = item.expiryDate;
-  let y = expDate.getFullYear();
-  let m = expDate.getMonth();
-  let d = expDate.getDate();
+const scheduleNotification = async(item) => {
+    let expDate = item.expiryDate;
+    let y = expDate.getFullYear();
+    let m = expDate.getMonth();
+    let d = expDate.getDate();
 
-  let scheduleDate = new Date(y, m, d - 5);
-  schedule.scheduleJob(`${item._id}`, scheduleDate, function () {
-    //send notification using nodemailer
-    //  await sendNotification()   // import from mail.js
+    let scheduleDate = new Date(y, m, d - 5);
+    schedule.scheduleJob(`${item._id}`, scheduleDate, function() {
+        //send notification using nodemailer
+        await sendNotification() // import from mail.js
 
-    console.log("Success");
-  });
+        console.log("Success");
+    });
 };
 
 const rescheduleNotification = (item) => {
-  let expDate = item.expiryDate;
-  let y = expDate.getFullYear();
-  let m = expDate.getMonth();
-  let d = expDate.getDate();
+    let expDate = item.expiryDate;
+    let y = expDate.getFullYear();
+    let m = expDate.getMonth();
+    let d = expDate.getDate();
 
-  let scheduleDate = new Date(y, m, d - 5);
-  schedule.rescheduleJob(`${item._id}`, scheduleDate, async function () {
-    //send notification using nodemailer
-    //  await sendNotification()   // import from mail.js
+    let scheduleDate = new Date(y, m, d - 5);
+    schedule.rescheduleJob(`${item._id}`, scheduleDate, async function() {
+        //send notification using nodemailer
+        await sendNotification() // import from mail.js
 
-    console.log("Success");
-  });
+        console.log("Success");
+    });
 };
 
 module.exports = {
-  scheduleNotification,
-  rescheduleNotification,
+    scheduleNotification,
+    rescheduleNotification,
 };
--- Frontend/config/mail.js
+++ Frontend/config/mail.js
@@ -1,3 +1,28 @@
-const sendNotification = () => {};
+const sendNotification = () => {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'uchihaitachi22093@gmail.com',
+            pass: 'konnoyar@55066'
+        }
+    });
+
+    var mailOptions = {
+        from: 'uchihaitachi22093@gmail.com',
+        to: "", //user email shd be accessed here 
+        subject: 'Sending Email using Node.js',
+        text: "your Product is gonna expire in so-so days"
+            // html: '<h1>Hi Smartherd</h1><p>Your Messsage</p>'        
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+};
+
 
 module.exports = { sendNotification };
