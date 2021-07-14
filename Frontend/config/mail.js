6c7c79c7ad677210ce1cd9fa6dce4f0b4ca733f7
rahul Sharma <sharmarahul1729@gmail.com>
2021-07-14 21:09:58 +0530

    mail

--- Frontend/config/mail.js
+++ Frontend/config/mail.js
@@ -12,7 +12,6 @@ const sendNotification = () => {
         to: "", //user email shd be accessed here 
         subject: 'Sending Email using Node.js',
         text: "your Product is gonna expire in so-so days"
-            // html: '<h1>Hi Smartherd</h1><p>Your Messsage</p>'        
     };
 
     transporter.sendMail(mailOptions, function(error, info) {
