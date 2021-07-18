15f6edadcedb932f59ef181b7e5a117b7551770b
rahul Sharma <sharmarahul1729@gmail.com>
2021-07-18 13:10:52 +0530

    fi

--- Frontend/config/helper.js
+++ Frontend/config/helper.js
@@ -1,35 +1,56 @@
 const schedule = require("node-schedule");
 const { sendNotification } = require('./mail');
+const orders = require('../models/temp');
 
-const scheduleNotification = async(item) => {
-    let expDate = item.expiryDate;
-    let y = expDate.getFullYear();
-    let m = expDate.getMonth();
-    let d = expDate.getDate();
+const schedule = async() => {
+    orders.find({}).then(result => {
+        let expDate = result.expiryDate;
 
-    let scheduleDate = new Date(y, m, d - 5);
-    schedule.scheduleJob(`${item._id}`, scheduleDate,async function() {
-        //send notification using nodemailer
-        await sendNotification() // import from mail.js
+        let month = expDate.getMonth();
+        let date = expDate.getDate();
+        let year = expDate.getFullYear();
 
-        console.log("Success");
-    });
-};
+        const todays_date = new Date();
+        const curr_month = todays_date.getMonth();
+        const curr_date = todays_date.getDate();
+        const curr_year = todays_date.getFullYear();
 
-const rescheduleNotification = (item) => {
-    let expDate = item.expiryDate;
-    let y = expDate.getFullYear();
-    let m = expDate.getMonth();
-    let d = expDate.getDate();
+        schedule.scheduleJob(`${result._id}`, `0 0 * * *`, () => {
 
-    let scheduleDate = new Date(y, m, d - 5);
-    schedule.rescheduleJob(`${item._id}`, scheduleDate, async function() {
-        //send notification using nodemailer
-        await sendNotification() // import from mail.js
+        })
 
-        console.log("Success");
-    });
-};
+    })
+}
+
+// const scheduleNotification = async(item) => {
+//     let expDate = item.expiryDate;
+//     let y = expDate.getFullYear();
+//     let m = expDate.getMonth();
+//     let d = expDate.getDate();
+
+//     let scheduleDate = new Date(y, m, d - 5);
+//     schedule.scheduleJob(`${item._id}`, scheduleDate,async function() {
+//         //send notification using nodemailer
+//         await sendNotification() // import from mail.js
+
+//         console.log("Success");
+//     });
+// };
+
+// const rescheduleNotification = (item) => {
+//     let expDate = item.expiryDate;
+//     let y = expDate.getFullYear();
+//     let m = expDate.getMonth();
+//     let d = expDate.getDate();
+
+//     let scheduleDate = new Date(y, m, d - 5);
+//     schedule.rescheduleJob(`${item._id}`, scheduleDate, async function() {
+//         //send notification using nodemailer
+//         await sendNotification() // import from mail.js
+
+//         console.log("Success");
+//     });
+// };
 
 module.exports = {
     scheduleNotification,
